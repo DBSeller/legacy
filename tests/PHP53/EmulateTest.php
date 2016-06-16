f49150f3b4c929085c0fb9dae67930c4ebb986b6
Rafael Nery <rafael.nery@dbseller.com.br>
2016-06-16 08:54:39 -0300

    Implementados mais testes

--- tests/PHP53/EmulateTest.php
+++ tests/PHP53/EmulateTest.php
@@ -25,8 +25,44 @@ class EmulateTest extends \PHPUnit_Framework_TestCase {
     $this->assertTrue(isset($GLOBALS['HTTP_POST_VARS']));
     $this->assertTrue(isset($GLOBALS['HTTP_COOKIE_VARS']));
     $this->assertTrue(isset($GLOBALS['HTTP_SERVER_VARS']));
-    $this->assertTrue(isset($GLOBALS['HTTP_SESSION_VARS']));
     $this->assertTrue(isset($GLOBALS['HTTP_FILES_VARS']));
-    
+  }
+
+  public function testRegisterGlobals() {
+
+    $_SERVER['oi'] = 'cara';
+    Emulate::registerGlobals();
+
+    global $oi;
+    $this->assertEquals($oi, $_SERVER['oi']);
+  }
+
+  public function testMargicQuotesGPCR() {
+
+    $original = "\\ '  \"";
+    $_COOKIE['teste2'] = $_GET['teste1'] = $original;
+
+    Emulate::magicQuotesGPCR("On");
+    $this->assertEquals("\\\\ \\'  \\\"", $_COOKIE['teste2']);
+
+    Emulate::magicQuotesGPCR("Off");
+    $this->assertEquals($original, $_COOKIE['teste2']);
+  }
+
+
+  public function testSessionRegister() {
+
+    session_register("TESTES_#1");
+
+    $this->assertTrue(
+      array_key_exists("TESTES_#1", $_SESSION)
+    );
+  }
+
+
+  public function testSessionIsRegistered() {
+
+    session_register("TESTES_#2");
+    $this->assertTrue(session_is_registered("TESTES_#2"));
   }
 }
